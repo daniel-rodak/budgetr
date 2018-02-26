274b7e5f8a10ffb4b0823ba9b94167d2a5fde0d4
Daniel Rodak <daniel.rodak1@gmail.com>
2018-02-26 15:17:09 +0100

    Limit QIF loading to Bank and Cash accounts

--- tests/testthat/test.readQIF.R
+++ tests/testthat/test.readQIF.R
@@ -2,3 +2,8 @@ test_that("QIF file loads properly",{
   test_format_QIF(qif)
   expect_equal(nrow(qif), 9)
 })
+
+test_that("Throw error on unsupported bank", {
+  expect_error(readQIF(file.path('..', 'testdata', 'invest.qif')),
+               "Currently only Bank and Cash accounts are supported. Contact with")
+})
